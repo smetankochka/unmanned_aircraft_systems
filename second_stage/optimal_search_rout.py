bef1793acdbb2ed44477c5c52824f3bbc704487b
smetanka <148964894+smetankochk@users.noreply.github.com>
2023-11-13 21:28:59 +0700

    Create optimal_search_rout.py

---
+++ second_stage/optimal_search_rout.py
@@ -0,0 +1,10 @@
+class solver:
+    def __init__(self, field_size):
+        # Задайте ваш конструктор
+        return
+
+    def solve(self, telemetry):
+        # Задайте ваш алгоритм управления
+        return v, roll, scan
+
+    # Можете задать дополнительные методы, для упрощения метода solve
